6ae0bf0ff43811397fc86dc067c8dd014ad784a5
langedechu <contact.nocteria@gmail.com>
2025-02-19 02:27:37 +0100

    refactor: streamline database query handling in getImage controller

--- controllers/getImage.js
+++ controllers/getImage.js
@@ -1,24 +1,25 @@
-const { db } = require("../index.js");
-const randomObject = require("../utils/randomObject.js");
-
 /**
  * Get a random entry from the database.
  * @param {import("express").Request} _req
  * @param {import("express").Response} res
  */
 function getImage(_req, res) {
-  const query = db.createQuery({
-    sql: "SELECT * FROM images",
-  });
+  const { db } = require("../index.js");
+  const randomObject = require("../utils/randomObject.js");
 
-  db.query(query, (err, results) => {
-    if (err) {
-      res.status(500).json({ error: "Internal server error." });
-      return;
+  db.query(
+    {
+      sql: "SELECT * FROM images",
+    },
+    (err, results) => {
+      if (err) {
+        res.status(500).json({ error: "Internal server error." });
+        return;
+      }
+
+      res.status(200).json(randomObject(results));
     }
-
-    res.status(200).json(randomObject(results));
-  });
+  );
 }
 
 module.exports = getImage;
